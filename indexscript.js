b8a61246edfd75136c76fb9f38c08baed534bacb
sebhq <141364733+sebhq@users.noreply.github.com>
2026-04-01 08:13:41 -0400

    Fix variable name in map.flyTo function

--- indexscript.js
+++ indexscript.js
@@ -16,7 +16,7 @@ fetch('houses.json')
           navigator.geolocation.getCurrentPosition(pos => {
             placeMarker([pos.coords.latitude, pos.coords.longitude],
               house.address, house.candy, true, house.outOfCandy);
-            map.flyTo(userCoords, 16);
+            map.flyTo(coords, 16);
           });
         }
       } else {
